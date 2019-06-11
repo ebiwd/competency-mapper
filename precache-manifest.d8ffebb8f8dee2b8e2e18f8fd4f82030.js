f09e6e397290b613a2c3c60e627479bdc014d980
Prakash Singh Gaur <prakash@ebi.ac.uk>
2019-06-11 14:12:24 +0100

    Updates

---
+++ precache-manifest.d8ffebb8f8dee2b8e2e18f8fd4f82030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c89bb9e2b2266b4b00a82e1045f3bf6",
+    "url": "/competency-mapper/index.html"
+  },
+  {
+    "revision": "53fd50a8fef156ff68bb",
+    "url": "/competency-mapper/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "67398ee3eb49d23c6d71",
+    "url": "/competency-mapper/static/css/main.19244f31.chunk.css"
+  },
+  {
+    "revision": "53fd50a8fef156ff68bb",
+    "url": "/competency-mapper/static/js/2.f14896c3.chunk.js"
+  },
+  {
+    "revision": "67398ee3eb49d23c6d71",
+    "url": "/competency-mapper/static/js/main.8ed5bf10.chunk.js"
+  },
+  {
+    "revision": "9612a556f0b300755501",
+    "url": "/competency-mapper/static/js/runtime~main.eaa69347.js"
+  }
+]);
