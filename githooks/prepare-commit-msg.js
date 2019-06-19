99b86a81c1499f31a98742b25e42c3ae4cff038a
Eduardo Sanz García <esanzgar@gmail.com>
2019-06-19 13:37:27 +0100

    chore: remove commit-prep-msg from shell to node

--- githooks/prepare-commit-msg.js
+++ githooks/prepare-commit-msg.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
-console.log(process.argv[2]);
+const fs = require('fs');
+
+const fileName = process.argv[2];
+const content = fs.readFileSync(fileName, 'utf-8');
+const firstLine = content.split('\n')[0];
+const hint = `#[feat|fix|docs|style|refactor|perf|test|chore]:-|
+
+#[what/why]
+
+`;
+
+if (firstLine === '') {
+  fs.writeFileSync(fileName, `${hint}${content}`);
+}
