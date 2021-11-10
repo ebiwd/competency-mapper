e0e025ed220585bf69db31daea98b8b7004665cc
prakashebi <prakash@ebi.ac.uk>
2021-11-10 10:49:03 +0000

    fixed loader gif

--- src/app/containers/courses/Courses.jsx
+++ src/app/containers/courses/Courses.jsx
@@ -263,7 +263,7 @@ class Courses extends Component {
             <img
               alt="progress"
               style={{ width: '7%' }}
-              src="../progressbar.gif"
+              src="/progressbar.gif"
             />
           </div>
         ) : (
