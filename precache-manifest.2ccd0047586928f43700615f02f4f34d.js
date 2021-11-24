bf2b857c91738c944779e26ee4b61f3f2db8d1a5
prakashebi <prakash@ebi.ac.uk>
2021-11-24 18:01:36 +0530

    Updates

---
+++ precache-manifest.2ccd0047586928f43700615f02f4f34d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56282e2587fce6d0c12f05a5aee7832e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f43ff3ec49386b61b651",
+    "url": "/static/css/2.07553d9e.chunk.css"
+  },
+  {
+    "revision": "911d428092ab6362ce6d",
+    "url": "/static/css/main.77517fbf.chunk.css"
+  },
+  {
+    "revision": "f43ff3ec49386b61b651",
+    "url": "/static/js/2.2717eec7.chunk.js"
+  },
+  {
+    "revision": "911d428092ab6362ce6d",
+    "url": "/static/js/main.be670b68.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9e7ef4d7a27321488c2b1ab5fd2cbd22",
+    "url": "/static/media/Bioexcel.9e7ef4d7.png"
+  },
+  {
+    "revision": "2b28bd091255b9475fb2a03e398c566f",
+    "url": "/static/media/PerMedCoE.2b28bd09.jpg"
+  },
+  {
+    "revision": "0ae786269b3bcec868b9118cd85c4360",
+    "url": "/static/media/european-union.0ae78626.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4195b3cb5df93e8ec8199e5ccda27747.js"
+  "/precache-manifest.2ccd0047586928f43700615f02f4f34d.js"
 );
 
 self.addEventListener('message', (event) => {
