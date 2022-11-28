3e40fb70f5a06c9d0a3259872e90cf09ffcfb443
Mahfouz Shehu <mahfouz@ebi.ac.uk>
2022-11-28 10:59:30 +0000

    putback withcredentials headerl confirmed it as the source of the problem;

--- src/app/services/util/auth.js
+++ src/app/services/util/auth.js
@@ -8,17 +8,23 @@ export default {
     is_logged_in: false,
     roles: []
   },
-  no_of_times_api_has_been_called: 0,
   getLoggedInUser() {
     return new Promise((resolve, reject) => {
       axios
         .get(
-          `${process.env.REACT_APP_HTTPS_CMS_API_URL}/api/users?_format=json`
+          `${process.env.REACT_APP_HTTPS_CMS_API_URL}/api/users?_format=json`,
+          {
+            withCredentials: true,
+            headers: {
+              Accept: 'application/hal+json',
+              'Content-Type': 'application/hal+json',
+              'X-CSRF-Token': this.csrf_token,
+              Authorization: 'Basic'
+            }
+          }
         )
         .then(response => {
           this.currently_logged_in_user = response.data.user;
-          this.no_of_times_api_has_been_called += 1;
-          // alert(this.no_of_times_api_has_been_called);
           resolve(response.data.user);
         })
         .catch(error => {
